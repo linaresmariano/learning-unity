22bc23ca0701fc955c78cb066c76ea8acd21770b
Mariano Linares <marianolinares92@gmail.com>
2015-01-06 12:01:38 -0300

    Interaccion con el teclado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,29 +11,24 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey("right") || Input.GetKey("d")) {
-			moving=1;
+			moving = -1;
 		} else if (Input.GetKey("left") || Input.GetKey("a")) {
-			moving=-1;
+			moving = 1;
 		} else {
-			moving=0;
+			moving = 0;
 		}
 
-		if (moving !=0) {
+		if (moving != 0) {
 			var velocityX = System.Math.Abs(rigidbody2D.velocity.x);
-
 			if (velocityX < .5) {
-
 				rigidbody2D.AddForce(new Vector2(moving, 0) * speed);
-
-				Debug.Log (this.transform.localScale.x);
-				if (this.transform.localScale.x > 0) {
-					//Vector3(moving, 1, 1);
+				if (this.transform.localScale.x != moving) {
+					float oldX = System.Math.Abs(this.transform.localScale.x);
+					this.transform.localScale = new Vector3(oldX * moving, this.transform.localScale.y, 1);
 				}
-
-				if (velocityX > maxSpeed) {
-			    	rigidbody2D.velocity = new Vector2(maxSpeed * moving, 0);
-			    }
 			}
+			if (velocityX > maxSpeed)
+				rigidbody2D.velocity = new Vector2(maxSpeed * moving, 0);
 		}
 	}
 }
